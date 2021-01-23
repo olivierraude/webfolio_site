761ca118f3baff3a448032bac28fbd93df460eaf
Olivier Raude <olivierraude@gmail.com>
2021-01-23 00:42:57 -0500

    fin setup+Route+Nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-/* import './App.css'; */
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import {
+  Project1,
+  Project2,
+  Project3,
+  Project4,
+  Project5,
+} from "./pages/Projects";
+import Contact from "./pages/Contact";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and start to code your webfolio.
-        </p>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/project-1" component={Project1} />
+      <Route exact path="/project-2" component={Project2} />
+      <Route exact path="/project-3" component={Project3} />
+      <Route exact path="/project-4" component={Project4} />
+      <Route exact path="/project-5" component={Project5} />
+      <Route exact path="/contact" component={Contact} />
+      <Redirect to="/" />
+    </Switch>
   );
-}
+};
 
 export default App;
