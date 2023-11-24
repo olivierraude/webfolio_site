67ea57a20a6122b9436f47eecbc3bbd624c578a1
olivierraude <olivierraude@gmail.com>
2023-11-24 11:08:05 -0500

    Ajout GA4

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,20 @@
 export const projectsData = [
   {
     id: 1,
+    titre: "Stranger Quiz",
+    categorie: "Programmation",
+    languages: ["Vanilla JS", "modules", "HTML", "SCSS", "SVG"],
+    description:
+      "Réalisation d'un quiz en Vanilla Js dans le cadre de ma formation. Ce travail a été réalisé individuellement, j'ai pu approfondir ma connaissance de JavaScript, des classes et de la gestion des modules. Le CSS est codé en SCSS et en respectant la méthode de nommage BEM.",
+    image: "./img/stranger_quiz.jpg",
+    alternative: "copie d'écran du projet Stranger Quiz",
+    width: "1920",
+    height: "950",
+    lien: "https://webfolio.olivierraude.com/stranger_quiz/",
+    github: "https://github.com/olivierraude/stranger_quiz",
+  },
+  {
+    id: 2,
     titre: "Site agence",
     categorie: "Programmation",
     languages: [
@@ -22,35 +36,7 @@ export const projectsData = [
     //github : ""
   },
   {
-    id: 2,
-    titre: "Kaliop",
-    categorie: "Programmation",
-    languages: ["HTML", "CSS", "JavaScript", "Parcel"],
-    description:
-      "Intégration d'une maquette en HTML, CSS et JavaScript en 24h pour l'agence Kaliop. Réalisation du projet avec le bundler Parcel.",
-    image: "./img/test_kaliop.jpg",
-    alternative: "copie d'écran du projet test",
-    width: "1920",
-    height: "950",
-    lien: "https://webfolio.olivierraude.com/test_kaliop/",
-    github: "https://github.com/olivierraude/email",
-  },
-  {
     id: 3,
-    titre: "Stranger Quiz",
-    categorie: "Programmation",
-    languages: ["Vanilla JS", "modules", "HTML", "SCSS", "SVG"],
-    description:
-      "Réalisation d'un quiz en Vanilla Js dans le cadre de ma formation. Ce travail a été réalisé individuellement, j'ai pu approfondir ma connaissance de JavaScript, des classes et de la gestion des modules. Le CSS est codé en SCSS et en respectant la méthode de nommage BEM.",
-    image: "./img/stranger_quiz.jpg",
-    alternative: "copie d'écran du projet Stranger Quiz",
-    width: "1920",
-    height: "950",
-    lien: "https://webfolio.olivierraude.com/stranger_quiz/",
-    github: "https://github.com/olivierraude/stranger_quiz",
-  },
-  {
-    id: 4,
     titre: "Webfolio",
     categorie: "Programmation",
     languages: ["ReactJS", "HTML", "SCSS", "GSAP"],
@@ -63,6 +49,29 @@ export const projectsData = [
     //lien : "Vous êtes ici",
     github: "https://github.com/olivierraude/webfolio_site",
   },
+  {
+    id: 4,
+    titre: "Pizzeria Ty Birill",
+    categorie: "Programmation",
+    languages: [
+      "Thème WP personnalisé",
+      "Bedrock",
+      "Gulp",
+      "Composer",
+      "SCSS",
+      "Vanilla JS",
+      "GSAP"
+    ],
+    description:
+      "Réalisation d'un thème Wordpress personnalisé pour le site de la pizzeria Ty Birill. Tout le contenu est administrable via ACF. Essentiel du JavaScript codé en Vanilla, utilisation de plusieurs librairies Javascript pour la gestion des animations et du slider, SwiperJS et Gsap. Gestion du dev front-end avec Gulp et les dépendances PHP avec Composer. Le CSS est codé en SCSS. Projet professionnel réalisé seul, DA, programmation, hébergement, SEO. Vous pouvez voir le détail du projet en suivant le lien Github. (en haut à droite!)",
+    image: "./img/tybirill.jpg",
+    alternative: "copie d'écran du projet Pizzeria Ty Birill",
+    width: "1920",
+    height: "950",
+    lien: "https://dev.olivierraude.me/tybirill/web",
+    github: "https://github.com/olivierraude/wordpress-tybirill",
+
+  },
   /*     {
         id : 5,
         titre : "Visuel pour email",
@@ -71,5 +80,17 @@ export const projectsData = [
         description : "Réalisation d'une typographie pour un email sur illustrator.",
         image : "./img/image-year-2021.png",
         lien : ""
-    }, */
+    }, 
+    id: 2,
+    titre: "Kaliop",
+    categorie: "Programmation",
+    languages: ["HTML", "CSS", "JavaScript", "Parcel"],
+    description:
+      "Intégration d'une maquette en HTML, CSS et JavaScript en 24h pour l'agence Kaliop. Réalisation du projet avec le bundler Parcel.",
+    image: "./img/test_kaliop.jpg",
+    alternative: "copie d'écran du projet test",
+    width: "1920",
+    height: "950",
+    lien: "https://webfolio.olivierraude.com/test_kaliop/",
+    github: "https://github.com/olivierraude/email",*/
 ];
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style/index.scss';
-// import './style/index.css';
+import './style/index.css';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
